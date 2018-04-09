cd24abd350fc90ab2c298b599eeb38d8e9cd2be7
rnicholus <github@raynicholus.com>
2018-04-08 19:14:42 -0500

    boilerplate

---
+++ dev/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="test">
+    vuejs-file-input manual testing
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'test',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style>
+</style>
